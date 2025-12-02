724e85692a0b56f370014b9d79ccdc7219ed7ab6
memayeux <memayeux@gmail.com>
2025-12-01 22:07:17 -0600

    task 1

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Mary Mayeux, Alan Leonard, Noel Fox
 
 Task code is found in files `taskx.py`. Output is in `outputx.csv`.
+
+These datasets are huge. Devs, see: https://git-lfs.com/ and track "*.csv"
--- task1.py
+++ task1.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
 # Task-1
 # Mary Mayeux
 
+# Extracting title, id, agent, body, repo_id, and repo_url from all_pull_request
+# and into a CSV file.
+
+"""
+Note about imports--write line 1 and line 12 one by one, running the script
+(python taskx.py) and installing whatever dependency is needed as the errors pop
+up. Needs pandas, fsspec, and huggingface_hub for sure.
+"""
+
+df = pd.read_parquet("hf://datasets/hao-li/AIDev/all_pull_request.parquet")
+
+# Remove unnecessary columns
+df = df.drop(columns=['number', 'user', 'user_id', 'state', 'created_at',
+    'closed_at', 'merged_at', 'html_url'])
+
+# Rename columns
+df = df.rename(columns={'id': 'ID', 'title': 'TITLE', 'body': 'BODYSTRING',
+                        'agent': 'AGENTNAME', 'repo_id': 'REPOID',
+                        'repo_url': 'REPOURL'})
+
+# Reorder columns
+df = df[['TITLE', 'ID', 'AGENTNAME', 'BODYSTRING', 'REPOID', 'REPOURL']]
+
+# Write data to CSV
+df.to_csv('output1.csv', index=False)
